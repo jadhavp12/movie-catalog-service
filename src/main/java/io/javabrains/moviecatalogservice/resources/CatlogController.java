2ee8656fbbe099c443b60729d43dc78b3c60b65c
jadhavp12 <jadhav.p12@gmail.com>
2021-09-29 11:10:49 +0530

    webclient call: replace resttemplate by webclient call

--- src/main/java/io/javabrains/moviecatalogservice/resources/CatlogController.java
+++ src/main/java/io/javabrains/moviecatalogservice/resources/CatlogController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import javax.annotation.Resource;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -21,21 +23,40 @@ public class CatlogController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
 public List<CatelogItem> getCatalog(@PathVariable("userId")  String userId){
+
+
         //get all rated movies
         List<Rating> ratings= Arrays.asList(
                 new Rating("1234",4),
                 new Rating("5678",3)
         );
-//        RestTemplate restTemplate=new RestTemplate();
+
+        //webclient call
+//        WebClient.Builder builder= WebClient.builder();//create object in main class
+
+//        RestTemplate restTemplate=new RestTemplate(); // resttemplate is creating object evey time so create object while loading project
 //        MovieItem movie= restTemplate.getForObject("http://localhost:8083/movies/"+ratings., MovieItem.class);
 //        restTemplate.getForObject("http://localhost:8082/rating/Stree",);
 
 
         //for each movie id get call movie info and get movie details
 return ratings.stream().map(rating -> {
-    MovieItem movie = restTemplate.getForObject("http://localhost:8083/movies/"+rating.getMovieId(), MovieItem.class) ;
+//    MovieItem movie = restTemplate.getForObject("http://localhost:8083/movies/"+rating.getMovieId(), MovieItem.class) ;
+
+    MovieItem movie= webClientBuilder.build()
+            .get()
+            .uri("http://localhost:8083/movies/"+rating.getMovieId())
+            .retrieve()
+            .bodyToMono(MovieItem.class)
+            .block();
+
+
+
     return new CatelogItem(movie.getMovieId(), movie.getMovieDesc(), rating.getRating());
 }).collect(Collectors.toList());
         //put them all together
